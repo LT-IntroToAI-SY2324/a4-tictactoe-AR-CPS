55b81aedf744788f7c7403ceee3a119556c4f0e9
AR-CPS <aaramos4@cps.edu>
2023-10-24 11:22:51 -0500

    changed to teacher version of defs

--- a4.py
+++ a4.py
@@ -11,16 +11,22 @@ class TTTBoard:
     """
 
     def __init__(self) -> None:
-        self.board = ["*"] * 9
+        self.board = ['*'] * 9
 
     def __str__(self) -> str:
         s = ""
-        for x in (0, 3, 6):
-            s += self.board(x+0) + " " + self.board(x+1) + self.board(x+2) 
+        for x in [0, 3, 6]:
+            s += self.board[x + 0] + " " + self.board[x + 1] + " " + self.board[x + 2] + "\n"
+        return s
 
-    def make_move(self, player, pos):
+    def make_move(self, player, pos) -> bool:
         if pos < 0 or pos > 8 or self.board[pos] != '*':
-            
+            return False
+
+        self.board[pos] = player
+        return True        
+    
+
 
 
 def play_tic_tac_toe() -> None:
